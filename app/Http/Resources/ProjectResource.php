2d73a320a53550c038d1462cd285dcab93bc35fd
MariamMohammed0011 <maream.mohamad.0938856942@gmail.com>
2025-04-09 05:07:17 -0700

    22

--- app/Http/Resources/ProjectResource.php
+++ app/Http/Resources/ProjectResource.php
@@ -17,7 +17,7 @@ class ProjectResource extends JsonResource
         return [
             'id' => $this->id,
             'projectTitle' => $this->project_title,
-            'imgPath' => asset($this->img_path), 
+           'imgPath' => asset('storage/' . $this->img_path),
             'categories' => CategoryResource::collection($this->whenLoaded('categories')),
         ];
     }
