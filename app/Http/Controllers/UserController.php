1cc7ab7f72f1efa03cc2d350ad6bc2a674eb3a11
MariamMohammed0011 <maream.mohamad.0938856942@gmail.com>
2025-04-12 14:08:26 -0700

    22

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,17 +4,29 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use App\Http\Resources\UserResource;
+use Illuminate\Support\Facades\Cache;
 
 class UserController extends Controller
 {
     public function index()
     {
-        $users = User::with('projects')->get();
+        // نخزن البيانات في الكاش لمدة 60 دقيقة
+        $users = Cache::remember('users_with_projects', 60, function () {
+            return User::with('projects')->get();
+        });
+
         return UserResource::collection($users);
     }
+
     public function show($id)
-{
-    $user = User::with('projects')->findOrFail($id); 
-    return new UserResource($user); 
-}
+    {
+        // كاش لكل مستخدم على حدة لتوفير أسرع استجابة
+        $cacheKey = 'user_with_projects_' . $id;
+
+        $user = Cache::remember($cacheKey, 60, function () use ($id) {
+            return User::with('projects')->findOrFail($id);
+        });
+
+        return new UserResource($user);
+    }
 }
